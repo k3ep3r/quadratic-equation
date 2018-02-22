87429f6512851a1ada299b48379a023a0bdc6f48
k3ep3r <keeper3211@gmail.com>
2018-02-22 23:25:50 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    equation = equation.replace(/\s/g,'');
+    var result = [],
+        array = equation.split(/\*x\^2|\*x/), //spliting our arr
+        a = parseInt(array[0]), //parsing array for int arg
+        b = parseInt(array[1]),
+        c = parseInt(array[2]),
+        discriminant = (Math.pow(b,2) - 4 * a * c); // discriminant func
+
+    result[0] = Math.round((-b + Math.sqrt(discriminant)) / (2 * a)); //result array
+    result[1] = Math.round((-b - Math.sqrt(discriminant)) / (2 * a)); //same
+
+    return result.sort((left, right) => left - right); //sort our result out with compr
 }
